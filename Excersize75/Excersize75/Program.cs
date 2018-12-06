477bfab3fa5af8b1b28b5af908fb942c9e504944
Jamil Abbas <jamilabbas787@gmail.com>
2018-12-05 21:55:25 -0500

    complete

---
+++ Excersize75/Excersize75/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersize75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            do
+            {
+                Console.Write("Enter a number: ");
+                string input = Console.ReadLine();
+                double number;
+                double.TryParse(input , out number);
+
+                if (number > 0)
+                {
+                    double root = number / 3;
+                    int i;
+                    for (i = 0; i < 32; i++)
+                        root = (root + number / root) / 2;
+                    Console.WriteLine($"Square root:{root}");
+                    break;
+                    
+                }
+                else
+                {
+                    continue;
+                }
+
+            } while (true);
+
+            Console.ReadKey();
+        }
+    }
+}
